ffeb72e46534b3b541d9732c0010ff44142cbfdd
klonkr <anton.cornett@gmail.com>
2021-12-15 09:00:52 +0100

    forgot which-key file

--- lua/config/which-key.lua
+++ lua/config/which-key.lua
@@ -3,33 +3,30 @@ local wk = require('whichkey_setup')
 local keymap = {
     
     ['<CR>'] = {'@q', 'macro q'}, -- setting a special key
-    t = {
-      name = '+telescope',
-      f = {
-        name = '+find',
-        f = 'Files',
-        b = 'Buffers',
-        g = 'Live grep',
-        h = 'Help Tags'
-      },
-      b = 'file browser',
-      g = {
-        name = '+git',
-        c = 'commits',
-        b = 'commits in buffer',
-        r = 'branches',
-        s = 'status',
-        t = 'stashes'
-      }
+    f = {
+      name = '+find',
+      f = 'Files',
+      b = 'Buffers',
+      g = 'Live grep',
+      h = 'Help Tags'
+      r = 'file browser'
     },
-    l = {
-      name = '+lsp',
-      s = {
-        name = '+symbols',
-        d = 'document',
-        w = 'workspace',
-        y = 'dYnamic'
-      },
+    g = {
+      name = '+git',
+      c = 'commits',
+      b = 'commits in buffer',
+      r = 'branches',
+      s = 'status',
+      t = 'stashes'
+    },
+    s = {
+      name = '+symbols',
+      d = 'document',
+      w = 'workspace',
+      y = 'dYnamic'
+    },
+    c = {
+      name = '+code',
       a = 'code actions',
       d = 'diagnostics',
       i = 'implementations',
